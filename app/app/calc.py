ebd5daeaf352394f379e814d6efba93f613b36d7
mustafa-tufekci <mustafa.tufekci.business@gmail.com>
2024-01-09 15:46:18 +0300

    Test files and postgresql docker configuration.

---
+++ app/app/calc.py
@@ -0,0 +1,11 @@
+"""
+Calculator functions
+"""
+
+def add(x, y):
+    """Ad x and y and retyrn result."""
+    return x + y
+
+def subtract(x, y):
+    """Subtract x from y and return result."""
+    return y - x
