3934553c85bbe7ebd26ba1ac593cdab766dce8e1
mustafa-tufekci <mustafa.tufekci.business@gmail.com>
2024-01-11 21:58:54 +0300

    Writes test for deleting tags and implement delete tag API

--- app/recipe/views.py
+++ app/recipe/views.py
@@ -39,9 +39,10 @@ class RecipeViewSet(viewsets.ModelViewSet):
         serializer.save(user=self.request.user)
 
 
-class TagViewSet(mixins.UpdateModelMixin,
+class TagViewSet(mixins.DestroyModelMixin,
+                 mixins.UpdateModelMixin,
                  mixins.ListModelMixin,
-                  viewsets.GenericViewSet):
+                 viewsets.GenericViewSet):
     """Manage tags in the database."""
     serializer_class = serializers.TagSerializer
     queryset = Tag.objects.all()
